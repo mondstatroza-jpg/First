d50f0120f2493725f04b08cb90232471d17cd6ec
mondstatroza-jpg <mondstatroza@gmail.com>
2026-09-20 18:51:17 +0700

    Вариант 1: День недели

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.first
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val edit1 = findViewById<EditText>(R.id.edit1)
+        val buttonOK = findViewById<Button>(R.id.buttonOK)
+        val textResult = findViewById<TextView>(R.id.textResult)
+
+        buttonOK.setOnClickListener {
+            val input = edit1.text.toString()
+            if (input.isEmpty()) {
+                textResult.text = "Введите число"
+                return@setOnClickListener
+            }
+            val number = input.toIntOrNull()
+            if (number == null) {
+                textResult.text = "Ошибка: введите целое число"
+                return@setOnClickListener
+            }
+            val day = when (number) {
+                1 -> "Понедельник"
+                2 -> "Вторник"
+                3 -> "Среда"
+                4 -> "Четверг"
+                5 -> "Пятница"
+                6 -> "Суббота"
+                7 -> "Воскресенье"
+                else -> "Неверный номер (1-7)"
+            }
+            textResult.text = day
         }
     }
 }
